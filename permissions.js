0791d724a96e767bfce398297e10f2c50ad028c4
Aman Muleva <chethanacl84@gmail.com>
2021-06-27 17:42:25 +0530

    web

---
+++ permissions.js
@@ -0,0 +1,14 @@
+const permissions = {
+  type: "service_account",
+  project_id: "fir-da6fb",
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY,
+  client_email: "firebase-adminsdk-nhzxw@fir-da6fb.iam.gserviceaccount.com",
+  client_id: "116616228977920918490",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-nhzxw%40fir-da6fb.iam.gserviceaccount.com",
+};
+export default permissions;
