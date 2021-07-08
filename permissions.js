674ab84f437e2f9b50d7861c2bc3b448bf1a6cc0
Aman Muleva <chethanacl84@gmail.com>
2021-07-08 15:51:05 +0530

    final

--- permissions.js
+++ permissions.js
@@ -1,8 +1,8 @@
 const permissions = {
   type: "service_account",
   project_id: "fir-da6fb",
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"),
   private_key_id: process.env.PRIVATE_KEY_ID,
-  private_key: process.env.PRIVATE_KEY,
   client_email: "firebase-adminsdk-nhzxw@fir-da6fb.iam.gserviceaccount.com",
   client_id: "116616228977920918490",
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
@@ -11,4 +11,5 @@ const permissions = {
   client_x509_cert_url:
     "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-nhzxw%40fir-da6fb.iam.gserviceaccount.com",
 };
+
 export default permissions;
